2a0209b4f6ead24f67b61dcdf08183d901ef39aa
ChaCharrier <charlotte.charrier97@gmail.com>
2023-03-29 12:14:34 +0200

    used props for quest react 4

--- pokedex/src/App.jsx
+++ pokedex/src/App.jsx
@@ -5,11 +5,23 @@ import './App.css'
 import PokemonCard from "./components/PokemonCard";
 
 function App() {
+  const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+
   return (
-  <div>
-      <PokemonCard/>
-  </div>
+    <div>
+      <PokemonCard pokemon={pokemonList[0]}/>
+    </div>
   );
 }
+
 
 export default App;
--- pokedex/src/components/PokemonCard.jsx
+++ pokedex/src/components/PokemonCard.jsx
@@ -1,5 +1,4 @@
-function PokemonCard() {
-  const pokemon = pokemonList[0];
+function PokemonCard({pokemon}) {
     return (
     <figure>
       {pokemon.imgSrc ? <img src= {pokemon.imgSrc} alt ={pokemon.name}></img> : <p>???</p> }
@@ -8,16 +7,5 @@ function PokemonCard() {
     );    
 }
 
-
-const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
 
 export default PokemonCard;
