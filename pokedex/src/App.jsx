f3c65f887d8c66c4dd0b61a4b16196817bb6269f
ChaCharrier <charlotte.charrier97@gmail.com>
2023-03-30 17:26:56 +0200

    quest react 6

--- pokedex/src/App.jsx
+++ pokedex/src/App.jsx
@@ -5,21 +5,42 @@ import './App.css'
 import PokemonCard from "./components/PokemonCard";
 import PropTypes from 'prop-types';
 
-function App() {
-  const pokemonList = [
-    {
-      name: "Bulbasaur",
+const pokemonList = [
+  {
+      name: "bulbasaur",
       imgSrc:
         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "charmander",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+    },
+    {
+      name: "squirtle",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+    },
+    {
+      name: "pikachu",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
     },
     {
       name: "mew",
     },
   ];
 
+function App() {
+  const [pokemonIndex, setPokemonIndex] = useState(0);
+  const handlePrev = () => setPokemonIndex(pokemonIndex -1);
+  const handleNext = () => setPokemonIndex(pokemonIndex +1);
+
   return (
     <div>
-      <PokemonCard pokemon={pokemonList[0]}/>
+      <PokemonCard pokemon={pokemonList[pokemonIndex]}/>
+      <button onClick={handlePrev}>Previous</button>
+      <button onClick={handleNext}>Next</button>
     </div>
   );
 }
