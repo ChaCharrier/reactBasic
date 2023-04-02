7a7e4fa4635a2262bbfbc0a82217f3f5546c74a8
ChaCharrier <charlotte.charrier97@gmail.com>
2023-04-02 16:29:23 +0200

    quest 7bis React

--- pokedex/src/App.jsx
+++ pokedex/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -34,6 +34,13 @@ const pokemonList = [
 
 function App() {
   const [pokemonIndex, setPokemonIndex] = useState(0);
+
+  useEffect(
+    () => {
+      alert ("Hello pokemon trainer :)");
+    },
+    []
+  )
 
   return (
     <div>
--- pokedex/src/components/NavBar.jsx
+++ pokedex/src/components/NavBar.jsx
@@ -2,12 +2,25 @@ const NavBar = ({pokemonIndex, setPokemonIndex}) => {
     const handlePrev = () => setPokemonIndex(pokemonIndex -1);
     const handleNext = () => setPokemonIndex(pokemonIndex +1);
 
+    if(pokemonIndex === 3) {
+        alert ("pika pikachu !!!");
+    }
+
 return (
     <div>
-        <button onClick={handlePrev}>Previous</button>
-        <button onClick={handleNext}>Next</button>
+        {/* <button 
+        key= {pokemonIndex.name}
+        name= {pokemonIndex.name}
+    /> */}
+    <button onClick={handlePrev}>Previous</button>
+    <button onClick={handleNext}>Next</button> 
     </div>
 )
 }
 
 export default NavBar;
+
+// créer un composant bouton qui sera appelé autant de fois qu'il y a de pokemon
+
+// boucler sur pokemonList dans le return du composant avec map, mettre des key uniques à chaque bouton
+ 
